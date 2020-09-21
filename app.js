674d4a300aedb7df93f22593edb121fffff9610c
Mac <tmcpbmay@gmail.com>
2020-09-21 10:21:31 -0400

    Make each file, add a few basics to some of them.

---
+++ app.js
@@ -0,0 +1,6 @@
+const base = 'https://gateway.marvel.com'
+const endpoint = '/v1/public/characters?name='
+const apikey = config.PUBLIC_KEY
+const privateKey = config.SECRET_KEY
+
+// example: http://gateway.marvel.com/v1/public/characters?name=${name}&ts=${ts}&apikey=${key}&hash=${hash}
